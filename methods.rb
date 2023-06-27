456f21698ca1691cc68ae098c680aedf143ff7dd
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-06-27 10:47:55 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet ("Chacha")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("darling")
+
+def add(num1, num2)
+    return  num1 + num2
+end
+puts add(1, 1)
+
+def halve(number)
+    if number.class != Integer
+        puts "please provide a number"
+        return nil
+    end
+    number / 2
+end
+puts halve("seven")
